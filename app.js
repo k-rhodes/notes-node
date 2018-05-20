12a4799d135e143dfc6648b7e307f8fd169de563
Kyle <rhodestraveler@gmail.com>
2018-05-19 23:01:12 -0500

    added notes file and required it in app.js, then added some properties to the moduleobject and called them in app.js

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ console.log('starting app');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
+var res = notes.addNote();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, function(err) {
-	if (err) {
-		console.log('Unable to write to file');
-	}
-});
+console.log(res);
+
+// var user = os.userInfo();
+
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function(err) {
+//	if (err) {
+//		console.log('Unable to write to file');
+//	}
+//});
 
 
