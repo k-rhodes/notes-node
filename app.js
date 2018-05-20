834d3c4cd14ee629c957b973932a0cdcaed784d2
Kyle <rhodestraveler@gmail.com>
2018-05-19 22:38:14 -0500

    learning 'require' to import node modules

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log('starting app');
+
+const fs = require('fs');
+const os = require('os');
+
+var user = os.userInfo();
+
+fs.appendFile('greetings.txt', `Hello ${user.username}!`, function(err) {
+	if (err) {
+		console.log('Unable to write to file');
+	}
+});
+
+
