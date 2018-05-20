aa8b1bc27996856d4fc7f5fb2a0a3bf7f3dc0694
Kyle <rhodestraveler@gmail.com>
2018-05-20 00:43:06 -0500

    require _uni1 in app.js, remove node_modules directory from root

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ const os = require('os');
 const _ = require('lodash');
 const notes = require('./notes.js');
 
-var res = notes.addNote();
 
-console.log('Results: ', notes.addNum(2,4));
+//console.log(_.isString(true));
+//console.log(_.isString("kyle"));
+
+var filteredArray = _.uniq(['kyle', 1, 'kyle', 1, 2, 3, 4, 5]);
+console.log(filteredArray);
+
+
+//console.log('Results: ', notes.addNum(2,4));
 
 // var user = os.userInfo();
 
