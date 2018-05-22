c6bbd7a22ff3df24e291bfce86577f3bbf3996c3
Kyle <rhodestraveler@gmail.com>
2018-05-21 23:31:43 -0500

    added logic to addNote() in notes.js to create new notes and add them to the notes array in notes-data.json, with prevention of saving duplcate notes

--- notes.js
+++ notes.js
@@ -1,13 +1,33 @@
 console.log('starting notes.js');
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-	console.log('adding note', title, body);
+	var notes = [];
+	var note = {
+		title,
+		body
+	};
+
+	try {
+		var noteString = fs.readFileSync('notes-data.json');
+		notes = JSON.parse(noteString);
+	} catch (e) {
+
+	}
+
+	var duplicateNotes = notes.filter((note) => note.title === title);
+
+	if (duplicateNotes.length === 0) {
+		notes.push(note);
+		fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+	}
 };
 
 var getAll = ()=>{
 	console.log('getting all notes');
 }
- 
+
 var getNote = (title) => {
 	console.log('Getting note title:', title);
 }
