d7236424b1816560722da67b1c5f370caedb25c6
Kyle <rhodestraveler@gmail.com>
2018-05-25 16:35:44 -0500

    read command and getNote() built. DRY utility function for addNote() and readNote() console.log() messages

--- app.js
+++ app.js
@@ -12,16 +12,23 @@ console.log('Command: ', command);
 console.log('Yargs.argv:', argv);
 
 if(command === 'add') {
-  note = notes.addNote(argv.title, argv.body);
+  var note = notes.addNote(argv.title, argv.body);
   if (note){
-    console.log('note created: ', note.title, note.body);
+    console.log('note created');
+    notes.logNote(note);
   } else {
     console.log('The title of that note is a duplicate.');
   }
 } else if (command === 'list') {
   notes.getAll();
 } else if (command === 'read') {
-  notes.getNote(argv.title);
+  var note = notes.getNote(argv.title);
+  if (note) {
+    console.log('note found');
+    notes.logNote(note);
+  } else {
+    console.log('Note not found.');
+  }
 } else if (command === 'remove') {
   var noteRemoved = notes.removeNote(argv.title);
   var message = noteRemoved ? 'Note was removed' : 'Note not found';
